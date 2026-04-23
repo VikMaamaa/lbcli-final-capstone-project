76e09bb5b80c4cbf6f3ffa3283f8b6a48a83198f
VikMaamaa <maamaavictor@yahoo.com>
2026-04-23 12:59:35 +0100

    task 6

--- submission/06.sh
+++ submission/06.sh
@@ -1,1 +1,14 @@
+#!/bin/bash
 # Only one tx in block 243,821 signals opt-in RBF. What is its txid?
+
+# Get the block hash of block 243,821
+target_block_hash=$(bitcoin-cli -signet getblockhash 243821)
+
+# Fetch the full block data with all transaction details (verbosity=2)
+target_block_data=$(bitcoin-cli -signet getblock $target_block_hash 2)
+
+# Find the transaction that signals opt-in RBF
+rbf_txid=$(echo $target_block_data | jq -r '.tx[] | select(.vin[].sequence <= 4294967293) | .txid')
+
+# Print the txid of the RBF signaling transaction
+echo $rbf_txid
