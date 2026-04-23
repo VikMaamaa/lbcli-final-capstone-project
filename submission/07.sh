a98bee85797a4442a88826c6436cfa50578e98e7
VikMaamaa <maamaavictor@yahoo.com>
2026-04-23 13:02:00 +0100

    task 7

--- submission/07.sh
+++ submission/07.sh
@@ -1,1 +1,11 @@
-# what is the coinbase tx in this block 243,834
+#!/bin/bash
+# What is the coinbase tx in block 243,834?
+
+# Get the block hash of block 243,834
+target_block_hash=$(bitcoin-cli -signet getblockhash 243834)
+
+# Fetch the block data and extract the coinbase transaction
+coinbase_txid=$(bitcoin-cli -signet getblock $target_block_hash | jq -r '.tx[0]')
+
+# Print the coinbase transaction id
+echo $coinbase_txid
