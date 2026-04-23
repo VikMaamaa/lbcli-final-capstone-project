9fb111795957189deae4c74a067babfae57e4095
VikMaamaa <maamaavictor@yahoo.com>
2026-04-23 12:54:45 +0100

    task 4

--- submission/04.sh
+++ submission/04.sh
@@ -1,1 +1,14 @@
 # Which public key signed input 0 in this tx: d948454ceab1ad56982b11cf6f7157b91d3c6c5640e05c041cd17db6fff698f7
+#!/bin/bash
+
+# Store the target transaction id
+target_txid=d948454ceab1ad56982b11cf6f7157b91d3c6c5640e05c041cd17db6fff698f7
+
+# Fetch the raw transaction data in decoded format (verbose=1)
+decoded_raw_tx=$(bitcoin-cli -signet getrawtransaction $target_txid 1)
+
+# Extract the public key from the witness data of input 0
+signing_pubkey=$(echo "$decoded_raw_tx" | jq -r '.vin[0].txinwitness[1]')
+
+# Print the signing public key
+echo $signing_pubkey
